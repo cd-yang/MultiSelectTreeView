424669e6e8062edba7a900f474f01ae9a5e34613
cd yang <1012892053@qq.com>
2020-04-30 17:51:16 +0800

    Add project files.

---
+++ MultiSelectTreeView/MainWindowViewModel.cs
@@ -0,0 +1,81 @@
+﻿using Prism.Mvvm;
+using System.Collections.ObjectModel;
+
+namespace MultiSelectTreeView
+{
+    class MainWindowViewModel : BindableBase
+    {
+        public ObservableCollection<TreeItem> TreeData { get; } = new ObservableCollection<TreeItem>();
+
+        public MainWindowViewModel()
+        {
+            AddTestData();
+        }
+
+        private void AddTestData()
+        {
+            TreeData.Add(new TreeItem
+            {
+                Data = new Test("1"),
+                Children = new ObservableCollection<TreeItem>
+                {
+                    new TreeItem
+                    {
+                        Data = new Test("1.1"),
+                        Children = new ObservableCollection<TreeItem>
+                        {
+                            new TreeItem
+                            {
+                                Data = new Test("1.1.1"),
+                            },
+                            new TreeItem
+                            {
+                                Data = new Test("1.1.2"),
+                            },
+                        }
+                    },
+                    new TreeItem
+                    {
+                        Data = new Test("1.2"),
+                    }
+                }
+            });
+            TreeData.Add(new TreeItem
+            {
+                Data = new Test("2"),
+                Children = new ObservableCollection<TreeItem>
+                {
+                    new TreeItem
+                    {
+                        Data = new Test("2.1"),
+                        Children = new ObservableCollection<TreeItem>
+                        {
+                            new TreeItem
+                            {
+                                Data = new Test("2.1.1"),
+                            },
+                            new TreeItem
+                            {
+                                Data = new Test("2.1.2"),
+                            },
+                        }
+                    },
+                    new TreeItem
+                    {
+                        Data = new Test("2.2"),
+                    }
+                }
+            });
+        }
+    }
+
+    public class Test
+    {
+        public string Label { get; set; }
+
+        public Test(string value)
+        {
+            Label = value;
+        }
+    }
+}
---
+++ MultiSelectTreeView/TreeItem.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Mvvm;
+using System.Collections.ObjectModel;
+
+namespace MultiSelectTreeView
+{
+    class TreeItem : BindableBase
+    {
+        public ObservableCollection<TreeItem> Children { get; set; }
+
+        public object Data { get; set; }
+
+        public bool IsSelected
+        {
+            get { return isSelected; }
+            set { SetProperty(ref isSelected, value); }
+        }
+
+        private bool isSelected = true;
+    }
+}
